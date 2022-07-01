0597b50d8790fa612e7254b34079145642f53f52
Sutharsan.S <78362509+s-sutharsan-20@users.noreply.github.com>
2022-07-01 13:16:32 +0530

    Add files via upload

---
+++ swaping of two variables.py
@@ -0,0 +1,7 @@
+a=int(input("Enter any Number :"))
+b=int(input("Enter another Number :"))
+print("Value of 'a' :",a)
+print("Value of 'b' :",b)
+a,b=b,a
+print("Value of 'a' after swaping :",a)
+print("Value of 'b' after swaping :",b)
