381fe9d51a15348d1cb761591db68ef463d7ce7b
Sutharsan.S <78362509+s-sutharsan-20@users.noreply.github.com>
2022-07-04 10:25:12 +0530

    Add files via upload

---
+++ built in math functions.py
@@ -0,0 +1,20 @@
+#built-in Mathematical functions
+#abs -- returns the absolute value
+x=-34
+y=3.43
+print(abs(x),abs(y))
+
+#ord --returns the ASCII value of  the given unicode character
+a='f'
+b='F'
+print(ord(a),ord(b))
+
+#chr --Returns the unicode character for the given ASCII value
+c=45
+d=38
+print(chr(c),chr(d))
+
+#bin --returns the binary string prefixed with '0b' for the given integer
+u=55
+v=85
+print(bin(u),bin(v))
